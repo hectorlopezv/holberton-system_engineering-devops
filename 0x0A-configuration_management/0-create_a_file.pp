39709648fb738f893289b441a53e0df9f5e2c2b3
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-08-05 21:46:41 -0500

    first try #1

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# creates a manifest for holberton file for server X
+file {
+  'holberton':
+  ensure => 'file',
+  path => '/tmp/holberton',
+  owner => 'www-data',
+  group => 'www-data',
+  content => 'I love Puppet',
+}
