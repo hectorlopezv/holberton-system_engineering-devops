8d11ecf8891923787ad622bb4ad05c971a09e8b2
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2020-08-11 14:14:54 -0500

    fix

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,27 @@
+
+package 
+{
+  'nginx':
+  ensure  => 'installed',
+}
+
+service
+{ 'nginx':
+  ensure  => running,
+  require => Package["nginx"],
+}
+
+
+file_line { 'sudo_rule':
+  ensure  => 'present',
+  path => '/etc/nginx/sites-available/default',
+  after => 'listen [::]:80 default_server;',
+  line => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+file {
+  'index.html':
+  ensure  => 'file',
+  path => '/var/www/html/',
+  content => 'Holberton School',
+}
