cd2443696b518d44cad23a77a6b44bebccf6ccf2
Hector Lopez <hectorvmlopez@gmail.com>
2020-09-14 13:25:39 -0500

    first commit

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import requests
+import sys
+"""consume data from api"""
+
+
+if __name__ == "__main__":
+    arg = sys.argv[1]
+    userId_request = requests.get(
+        'https://jsonplaceholder.typicode.com/users?id=' +
+        str(arg)).json()
+    false_todos = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId=' +
+        str(arg) +
+        '&completed=false').json()
+    true_todos = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId=' +
+        str(arg) +
+        '&completed=true').json()
+    name = userId_request[0]["name"]
+    done_task = len(true_todos)
+    false_task = len(false_todos)
+    total = done_task + false_task
+    print('Employee {} is done with tasks({}/{}):'.format(name,
+                                                          done_task, total))
+
+    for task in true_todos:
+        print('\t {}'.format(task["title"]))
---
+++ 0x15-api/README.md
@@ -0,0 +1,1 @@
+how TO consume API and transform data for our need
