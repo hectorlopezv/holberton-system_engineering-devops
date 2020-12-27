c5bcf9a33ded1c96b1efcb6d6a2a9a79d6652786
hector vinicio lopez <hectorvmlopez@gmail.com>
2020-12-27 15:45:21 -0500

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,15 +1,9 @@
-# customizing HTTP header with Puppet.
-exec {'update':
-  command => '/usr/bin/apt-get update',
-}
-package {'nginx':
-  ensure => 'present',
-}
-file_line { 'header':
-  path  => '/etc/nginx/nginx.conf',
-  match => 'http {',
-  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
-}
-exec {'start':
-  command => '/usr/sbin/service nginx start',
+#!/usr/bin/env bash
+#task advance with pupppet
+exec { 'http header':
+	command  => 'sudo apt-get update -y;
+	sudo apt-get install nginx -y;
+	sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
+	sudo service nginx restart',
+	provider => shell,
 }
